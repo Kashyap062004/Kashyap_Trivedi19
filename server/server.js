37fe7dbb574db6497b6e243acf886650c68573a2
Kashyap062004 <kashyap.trivedi2004@gmail.com>
2025-07-11 01:05:36 +0530

    deployments

--- server/server.js
+++ server/server.js
@@ -17,5 +17,9 @@ mongoose.connect(process.env.MONGO_URI, {
 const contactRoutes = require('./routes/contactRoutes');
 app.use('/api/contact', contactRoutes);
 
+app.get("/",  (req, rep) => {
+  rep.send("Hello World");
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
