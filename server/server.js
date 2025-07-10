61a632e7fd26e88ca78a6392dda4266dca0202d7
Kashyap062004 <kashyap.trivedi2004@gmail.com>
2025-07-11 01:18:25 +0530

    deployments

--- server/server.js
+++ server/server.js
@@ -21,5 +21,5 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = 8000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
