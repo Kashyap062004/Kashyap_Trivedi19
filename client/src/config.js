f63942b507ee6c8a80fce036400425e3017b301e
Kashyap062004 <kashyap.trivedi2004@gmail.com>
2025-07-11 01:20:26 +0530

    deployments

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    BACKEND_API: 'http://localhost:5000/api' // Replace with your actual backend API URL
+    BACKEND_API: 'https://portfolio-backend-api-skvl.onrender.com/api' // Replace with your actual backend API URL
 };
 
 export default config;
